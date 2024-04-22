7a663596a60c025ac971f04fd067832ae64aff07
Howard 李瀚文 <120554223+Howardlihanwen@users.noreply.github.com>
2024-04-22 11:27:35 +0800

    Create Howoldyouare.py

---
+++ Howoldyouare.py
@@ -0,0 +1,22 @@
+import datetime
+import calendr  # import calender 模組
+today = datetime.date.today()
+age = input('輸入生日 ( YYYY/MM/DD )：')
+age_list = age.split('/')
+year = today.year - int(age_list[0])
+month = today.month - int(age_list[1])
+if month<0:
+    year = year - 1
+    month = 12 + month
+day_list = [31,28,31,30,31,30,31,31,30,31,30,31]
+if calendr.isleap(today.year):     # 判斷如果是閏年
+    day_list[1] = 29                # 就將二月份改為29天
+day = today.day - int(age_list[2])
+if day<0:
+    month = month - 1
+    if month<0:
+        year = year - 1
+        month = 12 + month
+    day = day_list[month] + day
+print(f'{year} 歲 {month} 個月 {day} 天')
+  
